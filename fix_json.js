3a45d12e35c6a1dc2e3f941e7682ae4f1291d5be
Make <markusp_holopainen@yahoo.com>
2024-07-04 14:48:48 +0300

    Added newlines to ends

--- fix_json.js
+++ fix_json.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const resource = process.argv[process.argv.length - 1];
 
-const overwrite = process.argv.findIndex((x) => x === "-o") >= 0;
+const overwrite = process.argv.findIndex((x) => x === '-o') >= 0;
 
 console.log(`Converting file ${resource}...`);
 const account = require(resource);
@@ -14,7 +14,7 @@ const mergeObjects = (obj1, obj2) => {
     const tempKey = k2[i];
     if (obj1[tempKey] === null || obj1[tempKey] === undefined) {
       map[tempKey] =
-        typeof obj2[tempKey] === "string"
+        typeof obj2[tempKey] === 'string'
           ? obj2[tempKey]
           : { ...obj2[tempKey] };
     } else {
@@ -27,7 +27,7 @@ const mergeObjects = (obj1, obj2) => {
 
 const newMap = Object.keys(account).reduce((map, key) => {
   const val = account[key];
-  const keys = key.split(".");
+  const keys = key.split('.');
   let newObj = {};
   for (let i = keys.length - 1; i >= 0; i--) {
     if (i === keys.length - 1) {
@@ -41,12 +41,12 @@ const newMap = Object.keys(account).reduce((map, key) => {
 
 //console.log("newMap: ", newMap);
 
-const arr = resource.split(".");
+const arr = resource.split('.');
 const newFileName = overwrite
   ? resource
-  : [".", ...arr.slice(0, arr.length - 1), "_new"].join("") + ".json";
-console.log("Writing JSON to", newFileName);
+  : ['.', ...arr.slice(0, arr.length - 1), '_new'].join('') + '.json';
+console.log('Writing JSON to', newFileName);
 
 fs.writeFileSync(newFileName, JSON.stringify(newMap, null, 2));
 
-console.log("File write successful.\n");
+console.log('File write successful.\n');
